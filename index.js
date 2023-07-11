c23bcacb4cff3e730b9987fd3c3d925f74640969
PALADKRI <paladugu-sai.krishna@capgemini.com>
2023-07-11 10:10:35 +0530

    updated docker-compose

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const client = redis.createClient({
 client.set('visits', 0)
 
 app.get('/', (req, res) => {
-  client.git('visits', (err, visits) => {
-    res.send('no. of visits is ' + visits)
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits ' + visits)
     client.set('visits', parseInt(visits) + 1)
   })
 })
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080')
+app.listen(8081, () => {
+  console.log('listening on port 8081')
 })
