9b7609711a027a06caa5b036f2cad9b0ad769696
PALADKRI <paladugu-sai.krishna@capgemini.com>
2023-07-11 10:44:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const client = redis.createClient({
 })
 client.set('visits', 0)
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits ' + visits)
-    client.set('visits', parseInt(visits) + 1)
-  })
-})
+// app.get('/', (req, res) => {
+//   client.get('visits', (err, visits) => {
+//     res.send('Number of visits ' + visits)
+//     client.set('visits', parseInt(visits) + 1)
+//   })
+// })
 
 app.listen(8081, () => {
   console.log('listening on port 8081')
